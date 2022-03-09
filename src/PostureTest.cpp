eb5cae4cb649f8f3a7b460a6bee527292fab4540
Pierre Gergondet <pierre.gergondet@gmail.com>
2022-03-09 12:03:43 +0800

    Send a fixed posture command to the robot and exit after 6 seconds

--- src/PostureTest.cpp
+++ src/PostureTest.cpp
@@ -9,6 +9,19 @@ PostureTest::PostureTest(mc_rbdyn::RobotModulePtr rm, double dt, const mc_rtc::C
 
 bool PostureTest::run()
 {
+  auto pt = getPostureTask(robot().name());
+  if(pt->iterInSolver() == 2)
+  {
+    pt->target({{"panda_joint1", {1.5}}});
+  }
+  if(pt->iterInSolver() == 3 * (1 / solver().dt()))
+  {
+    pt->target({{"panda_joint1", {-1.5}}});
+  }
+  if(pt->iterInSolver() == 6 * (1 / solver().dt()))
+  {
+    std::exit(1);
+  }
   return mc_control::fsm::Controller::run();
 }
 
